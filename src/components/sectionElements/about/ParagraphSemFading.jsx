954400fb5f526f849ea9f69ad37ee91af4c01ee7
Gabriel de Souza da Silva <gabrielsouza65115@gmail.com>
2025-08-29 16:17:36 -0300

    fix: About e AboutInstagram

--- src/components/sectionElements/about/ParagraphSemFading.jsx
+++ src/components/sectionElements/about/ParagraphSemFading.jsx
@@ -1,11 +1,15 @@
 import Paragraphs from "../Paragraphs";
 import { useTranslation } from "react-i18next";
+import { Trans } from "react-i18next";
 
 export default function ParagraphSemFading() {
   const { t } = useTranslation();
 
   return (
     <Paragraphs className="text-colorWhite text-opacity-80">
+      {/* Renderiza HTML/JSX do JSON */}
+      <Trans i18nKey="about.paragraph" />
+
       {t("about.paragraph")}
     </Paragraphs>
   );
